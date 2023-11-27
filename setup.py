36c85ebd8727a5fb651650ed389f2fbecf14147d
ducdetronquito <g.paulet@smartway.ai>
2023-11-27 10:32:01 +0100

    chore: Bump version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="magicparse",
-    version="0.9.0",
+    version="0.9.1",
     description="Declarative parser",
     author="ZG",
     author_email="dev@zero-gachis.com",
