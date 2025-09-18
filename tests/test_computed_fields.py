375ce9cfc2c9f29f90c1a7b7e9672b6b40455f93
François Perin <f.perin@smartway.ai>
2025-09-18 14:53:51 +0200

    chore: Use pytest to assert exception

--- tests/test_computed_fields.py
+++ tests/test_computed_fields.py
@@ -6,15 +6,15 @@ from unittest import TestCase
 
 class TestBuild(TestCase):
     def test_without_builder(self):
-        with self.assertRaises(KeyError):
+        with pytest.raises(KeyError):
             ComputedField("output", {"type": "str"})
 
     def test_not_iterable_value_for_builder(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ComputedField("output", {"type": "str", "builder": 1})
 
     def test_bad_value_for_builder(self):
-        with self.assertRaises(ValueError):
+        with pytest.raises(ValueError):
             ComputedField("output", {"type": "str", "builder": "really"})
 
     def test_with_valid_builder(self):
